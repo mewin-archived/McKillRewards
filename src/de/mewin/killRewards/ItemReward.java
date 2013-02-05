a7c0392f85e361811d19408e496fe12b59b49444
mewin <mewin001@hotmail.de>
2013-02-05 20:06:04 +0100

    removed unused import

--- src/de/mewin/killRewards/ItemReward.java
+++ src/de/mewin/killRewards/ItemReward.java
@@ -25,7 +25,6 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.material.MaterialData;
 
 /**
  *
